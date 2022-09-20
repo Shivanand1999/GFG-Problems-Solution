e0d205705d2a391e679fc7fc78c360585991cf1f
Shivanand <73015307+Shivanand1999@users.noreply.github.com>
2022-09-20 17:52:49 +0530

    Add files via upload

---
+++ Sum_Of_First_N_Terms.cpp
@@ -0,0 +1,32 @@
+//Given and integer N.Calculate the sum of series 13 + 23 + 33 + 43 + … till N-th term.
+
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    long long sumOfSeries(long long N) {
+        // code here
+        long long temp = (N*(N+1)/2);
+        long long res = temp*temp;
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.sumOfSeries(N) << "\n";
+    }
+}
+// } Driver Code Ends
