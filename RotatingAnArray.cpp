6582429639f83d8d6925535963b0a6b8575176ca
Shivanand <73015307+Shivanand1999@users.noreply.github.com>
2022-09-24 16:31:28 +0530

    Add files via upload

---
+++ RotatingAnArray.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    void leftRotate(int arr[], int n, int d) {
+        // code here
+        Reverse(arr, 0, d-1);
+        Reverse(arr, d, n-1);
+        Reverse(arr, 0, n-1);
+    }
+    
+    void Reverse(int arr[], int start, int end){
+        while(start<=end){
+            swap(arr[start], arr[end]);
+            start++;
+            end--;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, d;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> d;
+        Solution ob;
+        ob.leftRotate(arr, n, d);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
